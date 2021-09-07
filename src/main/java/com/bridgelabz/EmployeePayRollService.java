78f0c230681cb8907ff1a2feb278aa2cbade68d3
Sakshi <sakshishetty1231997@gmail.com>
2021-09-07 21:54:31 +0530

    Added: Welcome message to the program

---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement Employee PayRoll Service System
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-09-07
+ */
+public class EmployeePayRollService {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the program");
+    }
+}
